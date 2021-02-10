9c368ee91686a98cf84b2860ec0a894152040095
[agokem] <ago@kemenesi.hu>
2021-02-10 08:58:18 +0100

    Video vége

--- 04_jQuery/script.js
+++ 04_jQuery/script.js
@@ -35,11 +35,46 @@ let azUjSzoveg = "Ezt jQuery-vel változtattuk meg."
 console.log($("h1").text(azUjSzoveg))
 
 console.log($(".sarga").css("background-color"))
-$("li").css("background-color", "lime")
-$(".sarga").css('backgorund-color', 'magenta')
+//$("li").css("background-color", "lime")
+//$(".sarga").css('backgorund-color', 'magenta')
 
 $("p").text('alma')
 
 $("p").html('alma, ami <strong>nagyon</strong> fontos')
 
 $("ul").append("<li>ujabb elem</li>")
+
+$("li:last-of-type").remove()
+
+function adjHozzaEgyLit() {
+    $("ul").append("<li>ujabb elem</li>")
+}
+
+function toroljEgyLit() {
+    $("li:last-of-type").remove()
+}
+
+$('#hozzaado').click(adjHozzaEgyLit)
+
+$('#torlo').click(toroljEgyLit)
+
+$("li:last-of-type").toggleClass('sárga')
+
+
+for (let i = 0; i < 100; i++) {
+    $("ul").append('<li> Nem csalok többet a vizsgán. </li>')
+}
+
+let szinek = ['orange', 'magenta', 'red', 'brown', 'green', 'blue', 'teal']
+
+function egySzinDoboz(szin) {
+  $('.kontener').append('<div class="doboz"></div>')
+  $('.kontener div:last-of-type').css('background-color', szin)
+}
+
+
+
+egySzinDoboz('lime')
+egySzinDoboz('pink')
+
+szinek.forEach(egySzinDoboz)
