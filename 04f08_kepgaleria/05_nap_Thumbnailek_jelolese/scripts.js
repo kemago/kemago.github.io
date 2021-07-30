ec8571cdb5d578884cbf47aa4eaf2b2b372dc915
Agoston Kemenesi <kemenesi@gmail.com>
2021-07-30 08:10:53 +0200

    Új mappa a végére
    Új mappa a végére

---
+++ 04f08_kepgaleria/05_nap_Thumbnailek_jelolese/scripts.js
@@ -0,0 +1,95 @@
+let currentPhoto = 0;
+
+// tömb a képek adataival
+let imagesData = [ 
+    {
+        photo: 'images/01.jpg',
+        title: 'Bora-Bora',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur voluptates voluptatibus nisi eaque illum aut itaque fugit nostrum, accusamus ratione optio placeat, aspernatur quia quasi ipsa sint unde assumenda harum?'
+        },
+    {
+        photo: 'images/02.jpg',
+        title: 'Sunset',
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere, sequi, dolorum cum error earum praesentium eos saepe nesciunt deleniti veniam iste natus, magnam facilis quasi porro voluptas nisi sed officia!'
+            
+    },
+    {
+        photo: 'images/03.jpg',
+        title: 'Beach',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos culpa neque eum molestiae eligendi, libero architecto perspiciatis aspernatur iste in sint at, ratione harum et eveniet rem. Facilis, repellat magni!'
+    },
+    {
+      photo: 'images/01.jpg',
+      title: 'Bora-Bora',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur voluptates voluptatibus nisi eaque illum aut itaque fugit nostrum, accusamus ratione optio placeat, aspernatur quia quasi ipsa sint unde assumenda harum?'
+    },
+    {
+      photo: 'images/02.jpg',
+      title: 'Sunset',
+      description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere, sequi, dolorum cum error earum praesentium eos saepe nesciunt deleniti veniam iste natus, magnam facilis quasi porro voluptas nisi sed officia!'      
+    },
+    {
+      photo: 'images/03.jpg',
+      title: 'Beach',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos culpa neque eum molestiae eligendi, libero architecto perspiciatis aspernatur iste in sint at, ratione harum et eveniet rem. Facilis, repellat magni!'
+    },
+    {
+      photo: 'images/01.jpg',
+      title: 'Bora-Bora',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur voluptates voluptatibus nisi eaque illum aut itaque fugit nostrum, accusamus ratione optio placeat, aspernatur quia quasi ipsa sint unde assumenda harum?'
+    },
+    {
+      photo: 'images/02.jpg',
+      title: 'Sunset',
+      description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere, sequi, dolorum cum error earum praesentium eos saepe nesciunt deleniti veniam iste natus, magnam facilis quasi porro voluptas nisi sed officia!'      
+    },
+    {
+      photo: 'images/03.jpg',
+      title: 'Beach',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos culpa neque eum molestiae eligendi, libero architecto perspiciatis aspernatur iste in sint at, ratione harum et eveniet rem. Facilis, repellat magni!'
+    }
+];
+
+// függvény a képek megjelenítéséhez
+// $('#photo').attr('src', imagesData[currentPhoto].photo);
+
+let loadImage = (photoNumber) => {
+    $('#photo').attr('src', imagesData[photoNumber].photo);
+    $('#photo-title').text(imagesData[photoNumber].title);
+    $('#photo-description').text(imagesData[photoNumber].description);
+};
+
+// a függvény futtatása
+loadImage(currentPhoto);
+
+// lapozó függvények
+$('#ra').click(() => {
+    if(currentPhoto < imagesData.length-1) {
+        currentPhoto++;
+    }
+    loadImage(currentPhoto);
+});
+
+$('#la').click(() => {
+    if(currentPhoto > 0) {
+        currentPhoto--;
+    }
+    loadImage(currentPhoto);
+});
+
+// thumbnail-ek megjelenítése
+imagesData.forEach((item,index) => {
+    // thumbnail-sorozat 
+    $('#thumbnails').append(`<div class="thumbnail" data-number="${index}">
+                                <img src="${item.photo}" alt="${item.title}" data-number="${index}"
+                            </div>`);
+    // kattintás egy thumbnail-en
+    $('.thumbnail').click((event)=>{
+        let photoNumber = parseInt($(event.target).attr('data-number'));
+        //let photoNumber = parseInt(indexClicked);
+        loadImage(photoNumber);
+    });
+    $( 'thumbnail' ).mouseover(function(event) {
+        $('.active').text($(event.target).attr('src'));
+    });
+});
