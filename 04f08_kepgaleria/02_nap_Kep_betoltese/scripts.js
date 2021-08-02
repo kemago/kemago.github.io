a57fc6a8c0258e070da865bd5a0ab3e8d5d90469
[agokem] <ago@kemenesi.hu>
2021-08-02 10:17:10 +0200

    új mappák
    mappák

--- 04f08_kepgaleria/02_nap_Kep_betoltese/scripts.js
+++ 04f08_kepgaleria/02_nap_Kep_betoltese/scripts.js
@@ -1,103 +1,55 @@
+// változó az aktuális kép tárolásához
 let currentPhoto = 0;
 
 // tömb a képek adataival
 let imagesData = [ 
     {
         photo: 'images/01.jpg',
-        title: '01_Bora-Bora',
+        title: 'Bora-Bora',
         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur voluptates voluptatibus nisi eaque illum aut itaque fugit nostrum, accusamus ratione optio placeat, aspernatur quia quasi ipsa sint unde assumenda harum?'
         },
     {
         photo: 'images/02.jpg',
-        title: '02_Sunset',
+        title: 'Sunset',
         description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere, sequi, dolorum cum error earum praesentium eos saepe nesciunt deleniti veniam iste natus, magnam facilis quasi porro voluptas nisi sed officia!'
             
     },
     {
         photo: 'images/03.jpg',
-        title: '03_Beach',
+        title: 'Beach',
         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos culpa neque eum molestiae eligendi, libero architecto perspiciatis aspernatur iste in sint at, ratione harum et eveniet rem. Facilis, repellat magni!'
     },
     {
       photo: 'images/01.jpg',
-      title: '04_Bora-Bora',
+      title: 'Bora-Bora',
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur voluptates voluptatibus nisi eaque illum aut itaque fugit nostrum, accusamus ratione optio placeat, aspernatur quia quasi ipsa sint unde assumenda harum?'
     },
     {
       photo: 'images/02.jpg',
-      title: '05_Sunset',
+      title: 'Sunset',
       description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere, sequi, dolorum cum error earum praesentium eos saepe nesciunt deleniti veniam iste natus, magnam facilis quasi porro voluptas nisi sed officia!'      
     },
     {
       photo: 'images/03.jpg',
-      title: '06_Beach',
+      title: 'Beach',
       description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos culpa neque eum molestiae eligendi, libero architecto perspiciatis aspernatur iste in sint at, ratione harum et eveniet rem. Facilis, repellat magni!'
     },
     {
       photo: 'images/01.jpg',
-      title: '07_Bora-Bora',
+      title: 'Bora-Bora',
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur voluptates voluptatibus nisi eaque illum aut itaque fugit nostrum, accusamus ratione optio placeat, aspernatur quia quasi ipsa sint unde assumenda harum?'
     },
     {
       photo: 'images/02.jpg',
-      title: '08_Sunset',
+      title: 'Sunset',
       description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere, sequi, dolorum cum error earum praesentium eos saepe nesciunt deleniti veniam iste natus, magnam facilis quasi porro voluptas nisi sed officia!'      
     },
     {
       photo: 'images/03.jpg',
-      title: '09_Beach',
+      title: 'Beach',
       description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos culpa neque eum molestiae eligendi, libero architecto perspiciatis aspernatur iste in sint at, ratione harum et eveniet rem. Facilis, repellat magni!'
     }
 ];
 
 // függvény a képek megjelenítéséhez
 // $('#photo').attr('src', imagesData[currentPhoto].photo);
-
-let loadImage = (photoNumber) => {
-    $('#photo').attr('src', imagesData[photoNumber].photo);
-    $('#photo-title').text(imagesData[photoNumber].title);
-    $('#photo-description').text(imagesData[photoNumber].description);
-};
-
-// a függvény futtatása
-loadImage(currentPhoto);
-
-// lapozó függvények
-$('#ra').click(() => {
-    if(currentPhoto < imagesData.length-1) {
-        currentPhoto++;
-    } else {
-    if(currentPhoto = imagesData.length) {
-        currentPhoto = 0;
-    }}
-    loadImage(currentPhoto);
-});
-
-$('#la').click(() => {
-    if(currentPhoto > 0) {
-        currentPhoto--;
-    } else {
-    if(currentPhoto == 0) {
-        currentPhoto = imagesData.length;
-    }}
-    loadImage(currentPhoto);
-});
-
-// thumbnail-ek megjelenítése
-imagesData.forEach((item,index) => {
-    // thumbnail-sorozat generálása
-    $('#thumbnails').append(`<div class="thumbnail" data-number="${index}">
-                                <img src="${item.photo}" alt="${item.title}" data-number="${index}"
-                            </div>`); //a keretre és a képre is kattinthat, ezért ismételjük meg a data-number-t
-    // kattintás egy thumbnail-en
-    $('.thumbnail').click((event)=>{ //ki kell olvasni, hogy hányadik képen kattintottak
-        let photoNumber = parseInt($(event.target).attr('data-number'));
-        //parseInt kovnertál stringből számmá
-        //let photoNumber = parseInt(indexClicked);
-        loadImage(photoNumber);
-    });
-    // a title jelenjen meg föléhúzáskor - nem működik még
-    $( 'thumbnail' ).mouseover(function(event) {
-        $('.active').text($(event.target).attr('src'));
-    });
-});
--- 04f08_kepgaleria/03_nap_Kepek_betoltese/scripts.js
+++ 04f08_kepgaleria/03_nap_Kepek_betoltese/scripts.js
@@ -4,48 +4,48 @@ let currentPhoto = 0;
 let imagesData = [ 
     {
         photo: 'images/01.jpg',
-        title: '01_Bora-Bora',
+        title: 'Bora-Bora',
         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur voluptates voluptatibus nisi eaque illum aut itaque fugit nostrum, accusamus ratione optio placeat, aspernatur quia quasi ipsa sint unde assumenda harum?'
         },
     {
         photo: 'images/02.jpg',
-        title: '02_Sunset',
+        title: 'Sunset',
         description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere, sequi, dolorum cum error earum praesentium eos saepe nesciunt deleniti veniam iste natus, magnam facilis quasi porro voluptas nisi sed officia!'
             
     },
     {
         photo: 'images/03.jpg',
-        title: '03_Beach',
+        title: 'Beach',
         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos culpa neque eum molestiae eligendi, libero architecto perspiciatis aspernatur iste in sint at, ratione harum et eveniet rem. Facilis, repellat magni!'
     },
     {
       photo: 'images/01.jpg',
-      title: '04_Bora-Bora',
+      title: 'Bora-Bora',
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur voluptates voluptatibus nisi eaque illum aut itaque fugit nostrum, accusamus ratione optio placeat, aspernatur quia quasi ipsa sint unde assumenda harum?'
     },
     {
       photo: 'images/02.jpg',
-      title: '05_Sunset',
+      title: 'Sunset',
       description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere, sequi, dolorum cum error earum praesentium eos saepe nesciunt deleniti veniam iste natus, magnam facilis quasi porro voluptas nisi sed officia!'      
     },
     {
       photo: 'images/03.jpg',
-      title: '06_Beach',
+      title: 'Beach',
       description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos culpa neque eum molestiae eligendi, libero architecto perspiciatis aspernatur iste in sint at, ratione harum et eveniet rem. Facilis, repellat magni!'
     },
     {
       photo: 'images/01.jpg',
-      title: '07_Bora-Bora',
+      title: 'Bora-Bora',
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur voluptates voluptatibus nisi eaque illum aut itaque fugit nostrum, accusamus ratione optio placeat, aspernatur quia quasi ipsa sint unde assumenda harum?'
     },
     {
       photo: 'images/02.jpg',
-      title: '08_Sunset',
+      title: 'Sunset',
       description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere, sequi, dolorum cum error earum praesentium eos saepe nesciunt deleniti veniam iste natus, magnam facilis quasi porro voluptas nisi sed officia!'      
     },
     {
       photo: 'images/03.jpg',
-      title: '09_Beach',
+      title: 'Beach',
       description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos culpa neque eum molestiae eligendi, libero architecto perspiciatis aspernatur iste in sint at, ratione harum et eveniet rem. Facilis, repellat magni!'
     }
 ];
@@ -53,51 +53,27 @@ let imagesData = [
 // függvény a képek megjelenítéséhez
 // $('#photo').attr('src', imagesData[currentPhoto].photo);
 
+// a függvény felépítése
 let loadImage = (photoNumber) => {
     $('#photo').attr('src', imagesData[photoNumber].photo);
     $('#photo-title').text(imagesData[photoNumber].title);
     $('#photo-description').text(imagesData[photoNumber].description);
 };
 
-// a függvény futtatása
+// a függvény futtatása (meg kell hívni)
 loadImage(currentPhoto);
 
 // lapozó függvények
 $('#ra').click(() => {
-    if(currentPhoto < imagesData.length-1) {
-        currentPhoto++;
-    } else {
-    if(currentPhoto = imagesData.length) {
-        currentPhoto = 0;
-    }}
-    loadImage(currentPhoto);
+  if(currentPhoto < imagesData.length-1) {
+      currentPhoto++;
+  }
+  loadImage(currentPhoto);
 });
 
 $('#la').click(() => {
-    if(currentPhoto > 0) {
-        currentPhoto--;
-    } else {
-    if(currentPhoto == 0) {
-        currentPhoto = imagesData.length;
-    }}
-    loadImage(currentPhoto);
-});
-
-// thumbnail-ek megjelenítése
-imagesData.forEach((item,index) => {
-    // thumbnail-sorozat generálása
-    $('#thumbnails').append(`<div class="thumbnail" data-number="${index}">
-                                <img src="${item.photo}" alt="${item.title}" data-number="${index}"
-                            </div>`); //a keretre és a képre is kattinthat, ezért ismételjük meg a data-number-t
-    // kattintás egy thumbnail-en
-    $('.thumbnail').click((event)=>{ //ki kell olvasni, hogy hányadik képen kattintottak
-        let photoNumber = parseInt($(event.target).attr('data-number'));
-        //parseInt kovnertál stringből számmá
-        //let photoNumber = parseInt(indexClicked);
-        loadImage(photoNumber);
-    });
-    // a title jelenjen meg föléhúzáskor - nem működik még
-    $( 'thumbnail' ).mouseover(function(event) {
-        $('.active').text($(event.target).attr('src'));
-    });
+  if(currentPhoto > 0) {
+      currentPhoto--;
+  }
+  loadImage(currentPhoto);
 });
